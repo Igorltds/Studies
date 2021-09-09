e78d64daf5ee383cddde4cb7bfd6217fa4bf2ff0
Igor Luan Teles de Souza <66261564+igorltds@users.noreply.github.com>
2021-09-09 02:22:41 -0300

    Delete Estuddos paralelos/sql_revisão directory

--- Estuddos paralelos/sql_revisão/querry_no_docker.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE Teste
-USE Teste
-CREATE TABLE Frutas(id int, nome VARCHAR(50), quantidade VARCHAR(3))
-INSERT INTO Frutas VALUES(1, 'manga',15)
-INSERT INTO Frutas VALUES(2, 'banana',30)
-GO
-SELECT * FROM Frutas WHERE quantidade > 16
